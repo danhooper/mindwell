42623f853ece1448b4bee042e9cc6a80774b1a12
Dan Hooper <dan.c.hooper@gmail.com>
2014-06-30 19:34:38 -0400

    Fixing path to files for tests

--- tests/unit_tests/__init__.py
+++ tests/unit_tests/__init__.py
@@ -1,8 +1,9 @@
 import sys
 import os
-sys.path.insert(0, os.path.abspath(os.path.join(__file__, '..', '..', 'google_appengine')))
-sys.path.insert(0, os.path.abspath(os.path.join(__file__, '..', '..', '..', 'mind-well')))
-sys.path.insert(0, os.path.abspath(os.path.join(__file__, '..', '..', '..', 'mind-well', 'reportlab.zip')))
+curr_dir = os.path.dirname(os.path.realpath(__file__))
+sys.path.insert(0, os.path.abspath(os.path.join(curr_dir, '..', '..', 'google_appengine')))
+sys.path.insert(0, os.path.abspath(os.path.join(curr_dir, '..', '..', 'mind-well')))
+sys.path.insert(0, os.path.abspath(os.path.join(curr_dir, '..', '..', 'mind-well', 'reportlab.zip')))
 sys.path.insert(0, '/var/lib/jenkins/google_appengine')
 os.environ['DJANGO_SETTINGS_MODULE'] = 'Mindwell.settings'
 import logging
