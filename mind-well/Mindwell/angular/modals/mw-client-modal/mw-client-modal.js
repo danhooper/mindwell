caaa9466bc908b2129e22630365a0bfda4f8b1fe
Dan Hooper <dan.c.hooper@gmail.com>
2015-03-29 13:46:19 -0400

    Added script when adding a new client

--- mind-well/Mindwell/angular/modals/mw-client-modal/mw-client-modal.js
+++ mind-well/Mindwell/angular/modals/mw-client-modal/mw-client-modal.js
@@ -1,9 +1,21 @@
-angular.module('mindwell').controller('MwClientModalCtrl', function($scope, mindwellUtil, $modalInstance) {
+angular.module('mindwell').controller('MwClientModalCtrl', function($scope, mindwellUtil, $modalInstance, mindwellCache) {
     $scope.client = {};
+
+    $scope.closeModal = function() {
+        $modalInstance.close($scope.client);
+    };
 
     $scope.saveClient = function() {
         mindwellUtil.saveClient($scope.client).then(function(client) {
-            $modalInstance.close(client);
+            $scope.client = client;
+            return mindwellCache.getCustomForm();
+        }).then(function(customForm) {
+            $scope.customForm = customForm;
+            if (customForm.new_client_script) {
+                $scope.showScript = true;
+            } else {
+                $scope.closeModal();
+            }
         });
     };
 
